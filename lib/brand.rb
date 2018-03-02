eafe5191917ee627e7744f39d9b2dbcec1c9d6b4
Emily Watkins <emily.e.dolliver@gmail.com>
2018-03-02 13:41:39 -0800

    add method to capitalize brand name, test passes.

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,3 +1,8 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
+  before_save(:capitalize_name)
+
+  def capitalize_name
+    self.name = name.split.map(&:capitalize).join(' ')
+  end
 end
