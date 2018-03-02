a8435c01908cac5a619a4fd51fa6e5d0319dc9db
Emily Watkins <emily.e.dolliver@gmail.com>
2018-03-02 13:37:40 -0800

    add method to capitalize name, test passes.

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,8 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many(:brands)
+  before_save(:capitalize_name)
+
+  def capitalize_name
+    self.name = name.split.map(&:capitalize).join(' ')
+  end
 end
