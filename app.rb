0fcfe4e89ae9fe0a083283bedd15a82c5ebdae84
Emily Watkins <emily.e.dolliver@gmail.com>
2018-03-02 10:34:12 -0800

    add store_edit template, test to check for update, added routing, passes test.

--- app.rb
+++ app.rb
@@ -23,6 +23,19 @@ get('/stores/:id') do
   erb(:store)
 end
 
+get('/stores/:id/edit') do
+  @store = Store.find(params['id'].to_i)
+  erb(:store_edit)
+end
+
+patch('/stores/:id') do
+  name = params['name']
+  @store = Store.find(params['id'].to_i)
+  @store.update({:name => name})
+  @stores = Store.all
+  erb(:store)
+end
+
 get('/brands') do
   @brands = Brand.all
   erb(:brands)
@@ -31,8 +44,8 @@ end
 post('/brands') do
   name = params['name']
   price = params['price']
-  @brand = Brand.create({:name => name, :price => price})
-  redirect("/brands/".concat(@brand.id.to_s))
+  brand = Brand.create({:name => name, :price => price})
+  redirect("/brands/".concat(brand.id.to_s))
 end
 
 get('/brands/:id') do
--- spec/store_pages_spec.rb
+++ spec/store_pages_spec.rb
@@ -8,3 +8,14 @@ describe 'the store creation path', {:type => :feature} do
     expect(page).to have_content('The Shoe Store')
   end
 end
+
+describe 'the store edit path', {:type => :feature} do
+  it 'takes the user to the stores edit page where they can edit a store' do
+    Store.create({:name => 'The Shoe Store'})
+    visit '/stores'
+    click_link('Update or Delete')
+    fill_in('name', :with => 'The New Shoe Store')
+    click_button('Update')
+    expect(page).to have_content('The New Shoe Store')
+  end
+end
