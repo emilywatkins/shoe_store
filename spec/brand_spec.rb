58e0f27f54995be34f0d61a7bae4f7c8c3d67788
Emily Watkins <emily.e.dolliver@gmail.com>
2018-03-02 13:40:07 -0800

    add test to check for brand name capitalization.

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -2,4 +2,11 @@ require('spec_helper')
 
 describe(Brand) do
   it { should have_and_belong_to_many(:stores) }
+
+  describe '#capitalize_name' do
+    it("capitalizes the first letter of each word in the name") do
+      brand = Brand.create({:name => 'the shoe brand'})
+      expect(brand.name).to(eq('The Shoe Brand'))
+    end
+  end
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -3,7 +3,7 @@ require('spec_helper')
 describe(Store) do
   it { should have_and_belong_to_many(:brands) }
 
-  describe '#capitalize' do
+  describe '#capitalize_name' do
     it("capitalizes the first letter of each word in the name") do
       store = Store.create({:name => 'the shoe store'})
       expect(store.name).to(eq('The Shoe Store'))
