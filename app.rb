4291929c879acc7bf074b136a42e09312d1e7af8
Emily Watkins <emily.e.dolliver@gmail.com>
2018-03-02 09:31:54 -0800

    add failing test for store creation, brands and stores templates, routing to pages.

--- app.rb
+++ app.rb
@@ -2,3 +2,15 @@ require("bundler/setup")
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  erb(:index)
+end
+
+get('/stores') do
+  erb(:stores)
+end
+
+get('/brands') do
+  erb(:brands)
+end
--- spec/store_pages_spec.rb
+++ spec/store_pages_spec.rb
@@ -1,1 +1,10 @@
 require('spec_helper')
+
+describe 'the store creation path', {:type => :feature} do
+  it 'takes the user to the stores page where they can create a store' do
+    visit '/stores'
+    fill_in('name', :with => 'The Shoe Store')
+    click_button('Add store')
+    expect(page).to have_content('The Shoe Store')
+  end
+end
