d9a8b4db467d5fd930df30512b994e0b50c17e3c
Emily Watkins <emily.e.dolliver@gmail.com>
2018-03-02 09:42:33 -0800

    add store template, add content to stores template, update route, test passes.

--- app.rb
+++ app.rb
@@ -8,7 +8,19 @@ get('/') do
 end
 
 get('/stores') do
+  @stores = Store.all
   erb(:stores)
+end
+
+post('/stores') do
+  name = params['name']
+  @store = Store.create({:name => name})
+  redirect("/stores/".concat(@store.id.to_s))
+end
+
+get('/stores/:id') do
+  @store = Store.find(params['id'].to_i)
+  erb(:store)
 end
 
 get('/brands') do
